1903224ad747e848b9b26618a1a574e2920dba29
Adebambo Adeshiyan <badeshiyan@gmail.com>
2021-02-02 08:35:45 -0500

    deleted app.css file and updated portfolio title

--- src/components/PortfolioTitle/PortfolioTitle.jsx
+++ src/components/PortfolioTitle/PortfolioTitle.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../PortfolioTitle/PortfolioTitle";
 
 const PortfolioTitle = () => {
-  return <h1 className="h1">Project</h1>;
+  return <h1 className="h1">Project:</h1>;
 };
 
 export default PortfolioTitle;
