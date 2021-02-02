502e31dc5ffc14ae4d0ebaa330b51c80b2b9a75d
Adebambo Adeshiyan <badeshiyan@gmail.com>
2021-02-01 21:59:58 -0500

    updated contact page

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -5,47 +5,6 @@ class Contact extends Component {
   render() {
     return (
       <>
-        <div className="container" style={{ padding: "55px" }}>
-          <div className="row">
-            <div className="col-12">
-              <div className="card">
-                <div className="card-body">
-                  <h5 className="card-title">Contact Me</h5>
-
-                  <input
-                    className="input"
-                    type="text"
-                    placeholder="Name"
-                    id="userName"
-                  />
-                  <br />
-                  <input
-                    className="input"
-                    type="text"
-                    placeholder="Email"
-                    id="userEmail"
-                  />
-                  <br />
-                  <input
-                    className="input"
-                    type="text"
-                    placeholder="Message"
-                    id="userMessage"
-                  />
-                  <br />
-
-                  <button
-                    className="btn btn-primary"
-                    style={{ marginTop: "10px" }}
-                  >
-                    Submit
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
         <main className="white-bg p-2">
           <h1 className="h1">Contact Information</h1>
           {/* <hr /> */}
